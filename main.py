920b3c4e0ba85f0d4bd13e02217435c21b64e864
Bilith <bilithvarghese10@gmail.com>
2026-02-11 16:58:51 +0100

    Initial system monitor app

---
+++ main.py
@@ -0,0 +1,9 @@
+import psutil
+
+cpu = psutil.cpu_percent()
+ram = psutil.virtual_memory().percent
+disk = psutil.disk_usage('/').percent
+
+print(f"CPU Usage: {cpu}%")
+print(f"RAM Usage: {ram}%")
+print(f"Disk: {disk}%")
